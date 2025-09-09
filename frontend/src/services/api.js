007097cff8b878d6bbb7d5476e95ba19ef9808e0
Manshi Rathour <manshi2086@gmail.com>
2025-09-09 16:53:31 +0530

    backend port changed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000", // FastAPI backend
+  baseURL: "http://127.0.0.1:5000", // FastAPI backend
 });
 
 // Attach token if available

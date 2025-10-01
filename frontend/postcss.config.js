efbcfea9f044a994e6cc47b7144b38ab59b74d47
Manshi Rathour <manshi2086@gmail.com>
2025-10-01 12:10:25 +0530

    tailwind css not working properly

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,5 +1,9 @@
+import tailwindPostcss from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
+
 export default {
-  plugins: {
-    '@tailwindcss/postcss': {},
-  },
-}
+  plugins: [
+    tailwindPostcss(),
+    autoprefixer(),
+  ],
+};
